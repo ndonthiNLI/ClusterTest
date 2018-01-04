3aa654367beef47f57ac54fec1a673094df18974
ndonthiNLI <ndonthi@next-level-integration.com>
2018-01-04 16:01:12 +0100

    start changes
    making changes in the starting part of the script

--- install_cluster.sh
+++ install_cluster.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# Copyright 2015 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-# # Unless required by applicable law or agreed to in writing, software # distributed under the License is distributed on an "AS IS" BASIS, # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-set -e
-
-function error_exit
+clear
+echo " welcome to GCE cluster installation, jenkins installation in k8s ns jenkins" 
+echo " relax for some minutes and get a coffee :) " 
+error_exit()
 {
     echo "$1" 1>&2
     exit 1
